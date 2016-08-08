2004eb1415863ecc96a7b3ffa517e311b43d06bd
brusinow <brusinow@me.com>
2016-08-07 23:49:51 -0700

    list items now showing properly from preset values

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import ToDoApp from './components/ToDoApp';
 
 class App extends React.Component {
-  render(){ // Every react component has a render method.
-    return( // Every render method returns jsx. Jsx looks like HTML, but it's actually javascript and functions a lot like xml, with self closing tags requiring the `/` within the tag in order to work propperly
+  render(){
+    return(
       <div>
         <ToDoApp />
       </div>
--- src/components/ToDoApp.js
+++ src/components/ToDoApp.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import List from './List';
+import Input from './Input';
 
 class ToDoApp extends React.Component {
-  render() {
+
+  componentWillMount(){
+    this.setState({
+      list: ['thing1', 'thing2', 'thing3']
+    })
+  };
+  onInputChange = (event) => {
+  this.setState({ newToDo: event.target.value}); // updates state to new value when user changes the input value
+  };
+
+  render(){
+    console.log('state', this.state)
     return (
-      <div>To Do App</div>
+      <div className="row">
+        <div className="col-md-10 col-md-offset-1">
+          <div className="panel panel-default">
+            <div className="panel-body">
+              <h1>My To Do App</h1>
+              <hr/>
+              <List listItems={this.state.list} />
+              <Input/>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
